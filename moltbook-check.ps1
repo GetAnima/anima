0dd5f593a33028a6b1a7ad31fccbc53d0a01d931
notmemoo <memomar168@gmail.com>
2026-01-30 00:38:17 -0800

    security: remove leaked API keys, add .gitignore for scripts

--- moltbook-check.ps1
+++
@@ -1,3 +0,0 @@
-$headers = @{ "Authorization" = "Bearer moltbook_sk_VPkGMqIIIETzsFxfeqF899NfGdvxiKAZ"; "Content-Type" = "application/json" }
-$response = Invoke-RestMethod -Uri "https://www.moltbook.com/api/v1/agents/status" -Headers $headers -Method Get
-$response | ConvertTo-Json -Depth 5
